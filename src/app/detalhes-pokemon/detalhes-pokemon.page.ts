4c45bfee3c0a0016a9c622a6876f8f57c7bdb6b7
vanessaaugutis <vanessaaugutis@gmail.com>
2023-10-14 12:49:13 -0300

    feature/tela-detalhes-pokemon

--- src/app/detalhes-pokemon/detalhes-pokemon.page.ts
+++ src/app/detalhes-pokemon/detalhes-pokemon.page.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: 'detalhes-pokemon.page.html',
-  styleUrls: []
+  styleUrls: ['detalhes-pokemon.page.scss']
 })
 export class DetalhesPokemonPage {
-  public informacoes = [];
+  public informacoes = {
+    name: '',
+    urlFoto: '',
+    peso: 0,
+    altura: 0,
+  };
 
-  constructor(private httpClient: HttpClient, private router: ActivatedRoute) {
+  constructor(private httpClient: HttpClient, private router: ActivatedRoute, private route: Router) {
     this.router.queryParams
       .subscribe(params => {
         this.getPokemonData(params['id']);
@@ -22,12 +27,19 @@ export class DetalhesPokemonPage {
   getPokemonData(id: string) {
     this.getPokemon(id)
       .subscribe((response) => {
-        console.log(response)
+        this.informacoes.name = response.name;
+        this.informacoes.urlFoto = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${id}.svg`;
+        this.informacoes.altura = response.height;
+        this.informacoes.peso = response.weight;
       });
 
   }
 
   getPokemon(id: string): Observable<any> {
     return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  }
+
+  returnPokemons() {
+    this.route.navigate(['']);
   }
 }
--- src/app/pokemons/pokemons.page.ts
+++ src/app/pokemons/pokemons.page.ts
@@ -17,6 +17,9 @@ export class PokemonsPage {
     }
   ];
 
+  public contagemPokemon = 0;
+  public pagination = 0;
+
   constructor(private httpClient: HttpClient, private router: Router) {
     this.getPokemonsData();
   }
@@ -26,7 +29,9 @@ export class PokemonsPage {
       .subscribe((response) => {
         this.pokemonsArray = response.results;
         this.pokemonsArray.forEach((pokemon) => {
-          const number = pokemon.url.charAt(pokemon.url.length - 2);
+          this.contagemPokemon+= 1;
+          let number = '';
+          number = this.contagemPokemon < 9 ? pokemon.url.charAt(pokemon.url.length - 2) : pokemon.url.charAt(pokemon.url.length - 3) + pokemon.url.charAt(pokemon.url.length - 2);
           pokemon.urlFoto = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${number}.svg`
         })
       });
@@ -34,14 +39,28 @@ export class PokemonsPage {
   }
 
   getPokemons(): Observable<any> {
-    return this.httpClient.get('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+    return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${this.pagination}`);
   }
 
   openDetalhes(url: string) {
-    const id = url.charAt(url.length - 2);
+    const id = this.contagemPokemon < 9 ? url.charAt(url.length - 2) : url.charAt(url.length - 3) + url.charAt(url.length - 2);
     this.router.navigate(
       ['pokemon'],
       { queryParams: { id: id } }
     );
+  }
+
+  changePagination(direcao: number) {
+    if ((this.pagination === 0) && (direcao === 0)) {
+      return;
+    }
+
+    if (direcao === 0) {
+      this.pagination -= 1;
+    } else {
+      this.pagination += 1;
+    }
+
+    this.getPokemonsData();
   }
 }
